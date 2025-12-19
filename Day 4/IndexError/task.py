8638dd3b2c6b42dec2204c5309a908593b4bc700
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-19 14:38:27 +0500

    Delete Day 4 directory

--- Day 4/IndexError/task.py
+++
@@ -1,8 +0,0 @@
-
-
-#Nested List = List within list, that's called Nested List.
-fruits = ["Cherry", "Apple", "Pear"]
-veg = ["Cucumber", "Kale", "Spinnach"]
-
-fruits_and_veg =[fruits,veg]
-print(fruits_and_veg)
