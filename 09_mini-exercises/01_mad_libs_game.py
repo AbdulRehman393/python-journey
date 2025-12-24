abea27f3ed416fa691a459940a4c2ccd7e81689e
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-24 05:16:43 +0500

    Add files via upload

---
+++ 09_mini-exercises/01_mad_libs_game.py
@@ -0,0 +1,14 @@
+# ----------------- Mad Libs Game----------------------------------
+# Where you have a story , but the user gets to submit nouns, verbs, and adjectives within your story.
+
+adjective1 = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+adjective2 = input("Enter ann adjective: ")
+verb = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exhibit, I saw {noun}")
+print(f"{noun} was {adjective2} and {verb}ing")
+print(f"I was {adjective3}")
---
+++ 09_mini-exercises/02_area_calc.py
@@ -0,0 +1,10 @@
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of a rectangle: "))
+height = float(input("Enter the height of a rectangle: "))
+
+area = length * width
+volume = length * width * height
+
+print(f"The area is: {area}")
+print(f"The volume is: {volume}")
