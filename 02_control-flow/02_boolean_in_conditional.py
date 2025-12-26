42da2cd8b1e234e3ef94248143696db809354ddf
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-26 22:47:03 +0500

    Add files via upload

---
+++ 02_control-flow/02_boolean_in_conditional.py
@@ -0,0 +1,7 @@
+
+online = True
+
+if online:                           # So, with if statements you could either write a condition or you could use a boolean
+    print("The user is online")
+else:
+    print("The user is offline")
