8d5626e9d9c3a30e679969ef64091932e176623a
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2026-01-21 20:00:40 +0500

    Add files via upload

--- 01_basics/01_display.py
+++ 01_basics/01_display.py
@@ -1,3 +1,11 @@
 #This is my first Python Program.
 print("I like Pizza")
 print("It's really good!")
+
+
+#To use the print() function, you pass the object you want to print as an argument to the function.
+# The print() function takes in any number of arguments, separated by a comma, and prints all of them.
+# For example, you can run the following code that prints a string, a variable, another string, and an integer together:
+
+month = "September"
+print("Investigate failed login attempts during", month, "if more than", 100)
