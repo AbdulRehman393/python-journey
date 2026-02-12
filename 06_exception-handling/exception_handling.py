5fd6c6cbdbb325404588de4a1e1818477f9c0eb7
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-02-12 03:32:36 -0800

    Add files via upload

--- 06_exception-handling/exception_handling.py
+++ 06_exception-handling/exception_handling.py
@@ -11,22 +11,30 @@
 # Exceptions will interrupt our program if they are not handled gracefully, here is how we can do that:
 
  # try:
- #     # Try some code, any code that's dangerous where it could cause an error, you will place it in a try block
+ #      Try some code, any code that's dangerous where it could cause an error, you will place it in a try block
  # e.g., any time you accept user input that is considered dangerous code because a user can, if exception happens
  # We will move to step 2
  # except Exception:
- #     # Handle an Exception
+ #      Handle an Exception
  # finally:
- #     # Do some clean up
+ #      will always execute regardless there's an exception or not. Do some clean up such as if you're handling files
+ # you may open a file , you want to be sure to close the file when you are done
 
 try:
     number = int(input("Enter a number: "))
     print(1/number)
 except ZeroDivisionError:
-    print("You can't divide by zero!")                # Some people do is that they catch all exceptions
-except Exception:
-    print("Something went wrong!")
+    print("You can't divide by zero!")
 except ValueError:
     print("Enter only numbers please!")
+except Exception:
+    print("Something went wrong!")
+finally:
+    print("Do some cleanup here")
 
 
+
+# Some people do is that they catch all exceptions, This is considered bad practice,
+# it's too broad it's good practice to tell the user what went wrong exactly.
+# except Exception:
+#    print("Something went wrong!")
