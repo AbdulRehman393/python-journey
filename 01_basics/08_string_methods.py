57ad9b879a300beac5401c334f22fd3af1298e3c
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-06-12 19:00:04 +0500

    Add files via upload

--- 01_basics/08_string_methods.py
+++ 01_basics/08_string_methods.py
@@ -14,9 +14,17 @@ print(result)
 result = name.rfind("o")     # If python is not able to locate a given character it will return negative one (-1)
 print(result)
 
-# Capitalize first character of a String
+# This method turns only the very first character of the
+# entire string into uppercase. Everything else in the string is forced into lowercase.
 result = name.capitalize()
 print(result)
+
+# This method turns the first letter of every
+# single word into uppercase, and makes the rest of the letters in those words lowercase.
+result = name.title()
+print(result)
+
+
 
 # upper method will take all characters in a string and make it upper case
 result = name.upper()
@@ -37,12 +45,47 @@ print(result)
 phone_number = input("Enter you phone number #:")
 
 # let's check how many characters (here we are checking dashes) in our string
-result = phone_number.count("0")
+result = phone_number.count("-")
 print(result)
 
-# replace method replace any occurence with one character with another, it's most useful method of string
+# replace method replaces any occurence with one character with another, it's most useful method of string
 result = phone_number.replace("-"," ")    # We can also eliminate dashes or another character by replacing it with
 print(result)                                          # empty string  like this  phone_number.replace("-","")
 
+
+# Only removes spaces at the start and end
+text = "  yes  "
+clean_text = text.strip()
+print(clean_text)  # Output: "yes"
+
+# Removes only leading whitespace (spaces at the start of the string).
+text = "  yes  "
+print(text.lstrip())  # Output: "yes  "
+
+
+# Removes only trailing whitespace (spaces at the end of the string).
+text = "  yes  "
+print(text.rstrip())  # Output: "  yes"
+
+
+# The .split() method splits a string into a list of smaller
+# strings (called substrings) based on a separator.
+text = "This is another example"
+words = text.split()
+print(words)
+
+
+print(" ".join(["This","is","a","sentence"]))
+
+
+test = "How-much-wood-would-a-woodchuck-chuck"
+print(test.split("-"))         # prints ['How', 'much', 'wood', 'would', 'a', 'woodchuck', 'chuck']
+
+
+print("-".join(test.split()))  # prints "How-much-wood-would-a-woodchuck-chuck"
+
+
+
 # If you want to comprehensive list to all the string methods available to you
-print(help(str))
+# print(help(str))
+
